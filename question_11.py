868caeb5448481556d94b00f96ade3d2cde83228
1999Arathy <85410988+1999Arathy@users.noreply.github.com>
2023-07-02 20:54:36 +0530

    Add files via upload

---
+++ question_11.py
@@ -0,0 +1,7 @@
+#Write a Python program to sum all the items in a list.
+sum=0
+lst = [25,45,96,35,78,26,12,10,100]
+for x in lst:
+    sum =sum+x
+
+print("sum of all the elements in list is :", sum)
